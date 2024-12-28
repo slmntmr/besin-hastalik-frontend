deff6863786398ceb000d2829022d4dd74a66f11
Süleyman TİMUR <slmn1905tmr@gmail.com>
2024-12-28 19:54:43 +0300

    vs

--- src/services/foodService.js
+++ src/services/foodService.js
@@ -2,13 +2,10 @@ import axios from 'axios';
 
 export const getRelatedDiseases = async (name) => {
   try {
-    const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_API_URL}/api/foods/${name}/related-diseases`
-    );
-    console.log("Gelen Veri:", response.data); // Gelen veriyi konsola yazdır
+    const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/foods/${name}/related-diseases`);
     return response.data;
   } catch (error) {
-    console.error("Hata:", error); // Hata varsa konsola yazdır
-    throw new Error(error.response?.data || "Bir hata oluştu.");
+    // Hata durumunda backend'den dönen mesajı fırlat
+    throw new Error(error.response?.data || 'Bir hata oluştu.');
   }
 };
