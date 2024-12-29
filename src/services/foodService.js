02c6323fbcee49e8b7c22ed6b05d49ed111893ab
Süleyman TİMUR <slmn1905tmr@gmail.com>
2024-12-29 14:49:35 +0300

    Yeni build oluşturuldu ve eski dosyalar temizlendi

--- src/services/foodService.js
+++ src/services/foodService.js
@@ -1,11 +1,11 @@
+import axios from 'axios';
+
 export const getRelatedDiseases = async (name) => {
   try {
-    console.log("Gönderilen isim:", name); // Parametreyi logla
     const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/foods/${name}/related-diseases`);
-    console.log("API Yanıtı:", response.data); // Yanıtı logla
     return response.data;
   } catch (error) {
-    console.error("Hata Detayı:", error.response?.data || error.message);
-    throw new Error(error.response?.data || "Bir hata oluştu.");
+    // Hata durumunda backend'den dönen mesajı fırlat
+    throw new Error(error.response?.data || 'Bir hata oluştu.');
   }
 };
